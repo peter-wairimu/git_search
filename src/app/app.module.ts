b63fce23f7fa45fef31c7d7b1da7dd21c5813997
peter-wairimu <peter.wairimu@student.moringaschool.com>
2021-08-26 18:06:33 +0300

    imported angular material cards

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,9 @@ import {MatIconModule} from '@angular/material/icon'
 import {MatFormFieldModule} from '@angular/material/form-field'
 import {MatSelectModule} from '@angular/material/select';
 import { SearchBarComponent } from './components/search-bar/search-bar.component'
+import {FlexLayoutModule} from '@angular/flex-layout'
+import {MatCardModule} from '@angular/material/card'
+import {MatButtonModule} from '@angular/material/button'
 @NgModule({
   declarations: [
     AppComponent,
